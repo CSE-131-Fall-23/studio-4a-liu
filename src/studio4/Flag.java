43ef0205b6f4efc707837f605c30847df9fd5a8b
cc <cc@LAPTOP-62D548SH.dhcp.wustl.edu>
2023-10-09 19:38:39 -0500

    finish

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,40 @@
 package studio4;
 
 import java.awt.Color;
-
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
-	public static void main(String[] args) {
-		
-	}
+
+    public static void main(String[] args) {
+        
+        StdDraw.setCanvasSize(800, 600);
+        StdDraw.setXscale(0, 800);
+        StdDraw.setYscale(0, 600);
+
+       
+        StdDraw.setPenColor(StdDraw.ORANGE);
+        StdDraw.filledRectangle(200, 300, 200, 300);
+
+    
+        StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
+        StdDraw.filledRectangle(600, 300, 200, 300);
+
+
+        StdDraw.setPenColor(StdDraw.GRAY);
+        StdDraw.filledRectangle(400, 300, 100, 50);
+
+   
+        StdDraw.setPenColor(StdDraw.DARK_GRAY);
+        StdDraw.filledRectangle(400, 325, 60, 25);
+
+       
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.filledCircle(340, 250, 20); 
+        StdDraw.filledCircle(460, 250, 20); 
+
+   
+        StdDraw.setPenColor(StdDraw.BLACK);
+        double thickness = 10;
+        StdDraw.rectangle(400, 300, 400 - thickness, 300 - thickness);
+    }
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -12,13 +13,45 @@ import edu.princeton.cs.introcs.StdDraw;
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
+
+
 public class InterpretDrawingFile {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		JFileChooser chooser = new JFileChooser("resources");
-		chooser.showOpenDialog(null);
-		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
-		
-	}
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(new File("path_to_your_txt_file")); // replace with your file path
+
+        String shapeType = in.next();
+        Color color = new Color(in.nextInt(), in.nextInt(), in.nextInt());
+        boolean isFilled = in.nextBoolean();
+
+        StdDraw.setPenColor(color);
+
+        switch (shapeType.toLowerCase()) {
+            case "rectangle":
+            case "ellipse":
+                double x = in.nextDouble();
+                double y = in.nextDouble();
+                double halfWidth = in.nextDouble();
+                double halfHeight = in.nextDouble();
+
+                if (shapeType.equalsIgnoreCase("rectangle")) {
+                    if (isFilled) StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+                    else StdDraw.rectangle(x, y, halfWidth, halfHeight);
+                } else {
+                    if (isFilled) StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+                    else StdDraw.ellipse(x, y, halfWidth, halfHeight);
+                }
+                break;
+
+            case "triangle":
+                double[] xs = {in.nextDouble(), in.nextDouble(), in.nextDouble()};
+                double[] ys = {in.nextDouble(), in.nextDouble(), in.nextDouble()};
+
+                if (isFilled) StdDraw.filledPolygon(xs, ys);
+                else StdDraw.polygon(xs, ys);
+                break;
+        }
+
+        in.close();
+    }
 }
